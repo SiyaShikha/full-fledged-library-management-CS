144815116fc9e4443de8fb48bba05a65818cb539
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2025-06-23 19:20:47 +0530

    modified GetRepository method to ensure create new InMemoryDatabase every time it's called

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -22,23 +22,25 @@ public class BookRepositoryTests
             .Options;
         
         var context = new LibraryContext(options);
+        
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        
         return new BookRepository(context);
     }
 
     [Fact]
     public async Task GetBooks_ReturnsAllBooks()
     {
-        // Arrange
         var repository = GetRepository();
-        await repository.AddBook(new Book { Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
-        await repository.AddBook(new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
-
-        // Act
+        await repository.AddBook(new Book { Id = 1, Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
+        await repository.AddBook(new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
+        
         var books = await repository.GetBooks();
-
-        // Assert
+        
         _testOutputHelper.WriteLine("hello");
         _testOutputHelper.WriteLine(books.ToString());
+        _testOutputHelper.WriteLine(books.Count().ToString());
         Assert.Equal(2, books.Count);
     }
 
@@ -46,14 +48,29 @@ public class BookRepositoryTests
     public async Task GetBook_ReturnsBookWithId()
     {
         var repository = GetRepository();
-        await repository.AddBook(new Book { Id = 100, Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
-        await repository.AddBook(new Book { Id = 200, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
+        await repository.AddBook(new Book { Id = 1, Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
+        await repository.AddBook(new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
         
-        var book = await repository.GetBook(100);
+        var book = await repository.GetBook(1);
         
-        Assert.Equal(100, book.Id);
+        Assert.Equal(1, book.Id);
         Assert.Equal("1984", book.Title);
         Assert.Equal("George Orwell", book.Author);
         Assert.Equal("Dystopian", book.Genre);
+    }
+    
+    [Fact]
+    public async Task DeleteBook_ReturnsTrue()
+    {
+        var repository = GetRepository();
+        await repository.AddBook(new Book { Id = 1, Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
+        await repository.AddBook(new Book { Id = 2, Title = "Book2", Author = "Author2", Genre = "Genre2" });
+        await repository.AddBook(new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
+        
+        var isDeleted = await repository.DeleteBook(1);
+        var books = await repository.GetBooks();
+        
+        Assert.True(isDeleted);
+        Assert.Equal(3, books.Count);
     }
 }
